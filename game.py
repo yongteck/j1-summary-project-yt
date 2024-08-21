91ecced15bff7c715f3f75aed86ac8946f5e3f1a
kaydnnn <160500192+kaydnnn@users.noreply.github.com>
2024-08-21 01:02:40 +0000

    yes

--- game.py
+++ game.py
@@ -5,32 +5,18 @@ class Game:
         self.phase = "explore"
         self.player = Player()
 
+    def isover(self):
+        return self.ended
+
     def CheckNode(self,room):
-        return room
+        return 
 
     def gameloop(self):
         print("game is running")
-        while self.phase != "end":
 
-        
-            if self.player.isdead:
-                self.phase = "end"
-                self.end = True
-            
-            if self.phase == "explore":
-                print("at node: ",self.player.playerpos)
-                next_rooms = GetNextRooms()
-                choice = int(input("Well done. Choose your next rooms: "))
-                for i in range(len(next_rooms)):
-                    print(f"({i+1}): room {next_rooms[i]}")
-                GoNextRoom(choice)
-            
-            elif self.phase == "battle":
-                print("fighting now")
-                
-            elif self.phase == "event":
-                print("kaydn bum event")
-            else:
-                print("Game ended")
-
-    
+        if self.phase == "explore":
+            print("at node: ",self.player.playerpos)
+        elif self.phase == "battle":
+            print("fighting now")
+        elif self.phase == "event":
+            print("kaydn bum event")
--- rooms.py
+++ rooms.py
@@ -1,5 +1,3 @@
-
-
 class Room:
     def __init__(self, type, id, nexts, monsters, items):
         self.type = type
@@ -8,6 +6,7 @@ class Room:
         self.next = None # Actual choice of the player
         self.monsters = monsters
         self.items = items
+        
 
     def CheckRoomType(self):
         return self.type
@@ -30,10 +29,13 @@ class Room:
 class Map:
     def __init__(self, head):
         self.current = head
+        
     def GetNextRooms(self):
         return self.current.nexts
+        
     def GoNextRoom(self, roomId):
         self.current = roomId
+        
     def GetRoomData(self):
         return [self.current.CheckRoomType(), self.current.CheckRoomMonsters(), self.current.CheckRoomItems()]
 
