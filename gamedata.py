bf7492a8fbfd14efcc345c74f2825804643ceb6e
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-07 05:48:05 +0000

    [feat] Create gamedata file and module

---
+++ gamedata.py
@@ -0,0 +1,31 @@
+"""gamedata.py
+
+Enables access to game data through the json file
+"""
+
+import json
+
+DATAFILE = "gamedata.json"
+
+
+def load(filename: str) -> dict:
+    """Loads game data from a json file"""
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
+
+def reload():
+    """Reload game data from file"""
+    data = load(DATAFILE)
+    global player, enemy, effects, rooms
+    player = data["player"]
+    enemy = data["enemy"]
+    effects = data["effects"]
+    rooms = data["rooms"]
+
+
+_data = load(DATAFILE)
+player = _data["player"]
+enemy = _data["enemy"]
+effects = _data["effects"]
+rooms = _data["rooms"]
