f4c8e59d7d19c8c07f31d1d9faeca46b75ca5b1d
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-10-09 00:44:05 +0000

    [feat] add ItemEffect class and factory function

--- inventory.py
+++ inventory.py
@@ -1,4 +1,47 @@
+import action
+import entities
 import gamedata
+
+
+class ItemEffect:
+    """An in-game item effect.
+
+    Items are distinct from Actions in two ways:
+    - Items only affect the item owner.
+    - Item effects are fixed-value boosts to stats, and do not involve
+      multipliers or other complicated calculations.
+    """
+    name: str
+    description: str
+
+    def __init__(self, change: dict[str, int]):
+        """Changes are represented as python dicts.
+        Keys: hp, maxhp, attack, defense, sanity
+        Values: int
+
+        Arguments
+        ---------
+        + change: dict[str, int]
+          The change to apply to the item owner.
+        """
+        self.change = change
+
+    def __str__(self):
+        return self.name
+
+    def apply_effect(self, entity: entities.Entity) -> None:
+        """Applies the effect of this item to the entity."""
+        for stat, value in self.change.items():
+            if stat == "maxhp":
+                entity.stats.maxhp += value
+            elif stat == "hp":
+                if value >= 0:
+                    entity.heal(value)
+                else:
+                    entity.take_hit(value)
+            else:
+                # Update the stats object attribute
+                setattr(entity.stats, stat, getattr(entity.stats, stat) + value)
 
 
 class Item:
@@ -63,3 +106,7 @@ class Inventory:
 def create_item(name: str) -> Item:
     itemdata = gamedata.items[name]
     return Item(itemdata["name"], itemdata["description"], itemdata["effects"], itemdata["states"])
+
+def create_item_effect(name: str) -> ItemEffect:
+    itemeffectdata = gamedata.itemeffects[name]
+    return ItemEffect(itemeffectdata)
